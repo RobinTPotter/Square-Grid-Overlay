030ec5475270d3197ef80adc488dfaf513cce5cc
robin <robin.potter@experian.com>
2018-10-12 18:52:49 +0100

    initial commit as a new project

---
+++ app/src/main/java/com/robin/squaregridoverlay/MainActivity.java
@@ -0,0 +1,308 @@
+package com.robin.squaregridoverlay;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int MY_PERMISSIONS_REQUEST_READ_PICS = 0;
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_PICS = 0;
+    private final int SELECT_PHOTO = 1;
+    Process process;
+    private boolean locked = false;
+    PictureView pictureView;
+    Uri imageUri;
+    int orientation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        permissionCheck();
+        try {
+
+            process = launchLogcat();
+            setContentView(R.layout.activity_main);
+            final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+
+            final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+
+            final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.setDrawerListener(toggle);
+            toggle.syncState();
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+
+
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    locked = !locked;
+                    if (locked) {
+                        Snackbar.make(view, "Locked", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+
+                        orientation= getRequestedOrientation();
+                        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+                    } else {
+                        Snackbar.make(view, "Unocked", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                        setRequestedOrientation(orientation);
+                    }
+                    pictureView.setStateLocked(locked);
+                }
+            });
+
+
+            pictureView = (PictureView) findViewById(R.id.pictureView);
+
+
+        } catch (Exception ex) {
+            Toast.makeText(this, "error: " + ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("colour", pictureView.getColour());
+        savedInstanceState.putInt("columns", pictureView.getC());
+        savedInstanceState.putInt("rows", pictureView.getR());
+        savedInstanceState.putFloat("mPosX", pictureView.getPosX());
+        savedInstanceState.putFloat("mPosY", pictureView.getPosY());
+        savedInstanceState.putFloat("mRotate", pictureView.getRotate());
+        savedInstanceState.putFloat("mScale", pictureView.getScale());
+        String stringUri="";
+        if (imageUri != null) stringUri=imageUri.toString();
+        savedInstanceState.putString("imageUri", stringUri);
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            pictureView.setColour(savedInstanceState.getInt("colour", Color.BLACK));
+            pictureView.setC(savedInstanceState.getInt("columns", 1));
+            pictureView.setR(savedInstanceState.getInt("rows",1));
+            pictureView.setPosX(savedInstanceState.getFloat("mPosX", 0f));
+            pictureView.setPosY(savedInstanceState.getFloat("mPosY", 0f));
+            pictureView.setRotate(savedInstanceState.getFloat("mRotate", 0f));
+            pictureView.setScale(savedInstanceState.getFloat("mScale", 1.0f));
+            imageUri = Uri.parse(savedInstanceState.getString("imageUri", ""));
+            loadPicture();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        if (locked) return true;
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_grid_2x2) {
+            pictureView.setRowsCols(2, 2);
+            pictureView.invalidate();
+            return true;
+        } else if (id == R.id.action_grid_3x3) {
+            pictureView.setRowsCols(3, 3);
+            pictureView.invalidate();
+            return true;
+        }else if (id == R.id.action_grid_4x4) {
+            pictureView.setRowsCols(4, 4);
+            pictureView.invalidate();
+            return true;
+        }else if (id == R.id.action_grid_5x5) {
+            pictureView.setRowsCols(5, 5);
+            pictureView.invalidate();
+            return true;
+        }else if (id == R.id.action_grid_4x3) {
+            pictureView.setRowsCols(4, 3);
+            pictureView.invalidate();
+            return true;
+        }else if (id == R.id.action_grid_3x4) {
+            pictureView.setRowsCols(3, 4);
+            pictureView.invalidate();
+            return true;
+        } else if (id == R.id.action_grid_colour_black) {
+            pictureView.setColour(Color.BLACK);
+            pictureView.invalidate();
+            return true;
+        } else if (id == R.id.action_grid_colour_yellow) {
+            pictureView.setColour(Color.YELLOW);
+            pictureView.invalidate();
+            return true;
+        } else if (id == R.id.action_image_rotate_mode) {
+            pictureView.setRotating(true);
+            pictureView.invalidate();
+            return true;
+        } else if (id == R.id.action_image_scale_mode) {
+            pictureView.setRotating(false);
+            pictureView.invalidate();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.nav_gallery) {
+            Intent selectImageIntent = new Intent(Intent.ACTION_PICK);
+            selectImageIntent.setType("image/*");
+            if (selectImageIntent.resolveActivity(getPackageManager()) != null) {
+                // Bring up gallery to select a photo
+                startActivityForResult(selectImageIntent, SELECT_PHOTO);
+            }
+           // Intent chooser = Intent.createChooser(selectImageIntent, "Choose Picture");
+          //  startActivityForResult(chooser, SELECT_PHOTO);
+
+
+
+
+
+
+
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch (requestCode) {
+            case SELECT_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    final Uri _imageUri = imageReturnedIntent.getData();
+                    imageUri = _imageUri;
+                    loadPicture();
+                }
+        }
+    }
+
+    public void loadPicture() {
+        try {
+            final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+            Bitmap bitmap = BitmapFactory.decodeStream(imageStream).copy(Bitmap.Config.ARGB_8888, true);
+            pictureView.setBitmap(bitmap);
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void permissionCheck() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_PICS
+                );
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            Toast.makeText(this, "permission granted", Toast.LENGTH_SHORT).show();
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_PICS
+                );
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            Toast.makeText(this, "permission granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private Process launchLogcat() {
+        try {
+            File filename = new File(Environment.getExternalStorageDirectory() + "/gridthing-logfile.log");
+            filename.createNewFile();
+            String cmd = "logcat -d -f " + filename.getAbsolutePath();
+            return Runtime.getRuntime().exec(cmd);
+        } catch (IOException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/robin/squaregridoverlay/PictureView.java
@@ -0,0 +1,335 @@
+package com.robin.squaregridoverlay;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.widget.Toast;
+
+public class PictureView extends View {
+
+
+    private static final float ROTATION_MULTIPLIER = 50.0f;
+    private int offsetx = 0;
+    private int offsety = 0;
+
+    private int bmpoffsetx = 0;
+    private int bmpoffsety = 0;
+    private int bmpwidth = 0;
+    private int bmpheight = 0;
+
+    private String currentPicture;
+    private Bitmap bitmap;
+
+    private int colour = Color.BLACK;
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
+
+    private int c = 0;
+    private int r = 0;
+
+    private boolean stateLocked = false;
+
+    public void setStateLocked(boolean l) {
+        stateLocked = l;
+    }
+
+    private float mLastTouchX;
+    private float mLastTouchY;
+    private static final int INVALID_POINTER_ID = -1;
+    private int mActivePointerId = INVALID_POINTER_ID;
+
+    private float mPosX;
+    private float mPosY;
+
+    private ScaleGestureDetector mScaleDetector;
+    private float mScaleFactor = 1.0f;
+    private float mRotate = 0.0f;
+    private boolean rotating = false;
+
+    public PictureView(Context context) {
+        super(context);
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+
+
+    }
+
+    public PictureView(Context context, AttributeSet attr) {
+        super(context, attr);
+        mScaleDetector = new ScaleGestureDetector(context, new ScaleListener());
+
+
+    }
+
+    public void setRotating(boolean r) {
+        rotating = r;
+    }
+
+    public void setBitmap(Bitmap bmp) {
+        bitmap = bmp;
+        bmpoffsetx = 0;
+        bmpoffsety = 0;
+        bmpwidth = bmp.getWidth();
+        bmpheight = bmp.getHeight();
+    }
+
+    public void setColour(int col) {
+        colour = col;
+    }
+
+    public void setScale(float sc) {
+        mScaleFactor = sc;
+    }
+
+    public void setPosX(float sc) {
+        mPosX = sc;
+    }
+
+    public void setPosY(float sc) {
+        mPosY = sc;
+    }
+
+    public void setRotate(float sc) {
+        mRotate = sc;
+    }
+
+    public int getColour() {
+        return colour;
+    }
+
+    public float getScale() {
+        return mScaleFactor;
+    }
+
+    public float getPosX() {
+        return mPosX;
+    }
+
+    public float getPosY() {
+        return mPosY;
+    }
+
+    public float getRotate() {
+        return mRotate;
+    }
+
+
+    public void setRowsCols(int rt, int ct) {
+        r = rt;
+        c = ct;
+    }
+
+    public void onDraw(Canvas canvas) {
+
+        super.onDraw(canvas);
+
+
+        try {
+            if (bitmap == null) {
+                Paint p = new Paint();
+                p.setStyle(Paint.Style.FILL);
+                p.setColor(Color.RED);
+                canvas.drawRect(new Rect(0, 0, getWidth(), getHeight()), p);
+                return;
+            } else if (!bitmap.isMutable()) {
+                Paint p = new Paint();
+                p.setStyle(Paint.Style.FILL);
+                p.setColor(Color.GRAY);
+                canvas.drawRect(new Rect(0, 0, getWidth(), getHeight()), p);
+                return;
+            }
+
+            boolean matrixStyle = true;
+
+            if (!matrixStyle) {
+
+                Rect src = new Rect(bmpoffsetx, bmpoffsety, bmpwidth, bmpheight);
+                double aspect_src = 1.0 * bmpwidth / bmpheight;
+                double aspect_dest = 1.0 * getWidth() / getHeight();
+                int destx = 0;
+                int desty = 0;
+                int destwidth = getWidth();
+                int destheight = getHeight();
+                if (aspect_src > aspect_dest) {
+                    //src wider, nudge down, calc height
+                    destheight = (int) (destwidth / aspect_src);
+                    desty = getHeight() / 2 - destheight / 2;
+                } else {
+                    //dest wider, nudge across calc width
+                    destwidth = (int) (destheight * aspect_src);
+                    destx = getWidth() / 2 - destwidth / 2;
+                }
+                Rect dst = new Rect(destx + (int) mPosX, desty + (int) mPosY, destwidth + destx + (int) mPosX, destheight + desty + (int) mPosY);
+                canvas.drawBitmap(bitmap, src, dst, null);
+            } else {
+                Matrix matrix = new Matrix();
+                matrix.reset();
+                matrix.postScale(mScaleFactor, mScaleFactor);
+                matrix.preRotate(mRotate);
+                matrix.postTranslate(mPosX / mScaleFactor, mPosY / mScaleFactor);
+                canvas.drawBitmap(bitmap, matrix, null);
+            }
+            //make sure grid goes in the centre
+
+
+            //Toast.makeText(this, "canvas is " + canvas.toString(), Toast.LENGTH_SHORT).show();
+
+            if (c > 0 & r > 0) {
+
+                Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+                paint.setStrokeWidth(1.0f);
+                paint.setStyle(Paint.Style.STROKE);
+                paint.setColor(colour);
+
+                //Toast.makeText(this, "set paints etc", Toast.LENGTH_SHORT).show();
+
+                //get max square size
+                int width = getWidth() / c;
+                int height = getHeight() / r;
+                if (width < height) height = width;
+                else width = height;
+
+                offsetx = getWidth() / 2 - (width * c) / 2;
+                offsety = getHeight() / 2 - (height * r) / 2;
+
+
+                //Toast.makeText(this, "" + width + "," + height, Toast.LENGTH_SHORT).show();
+
+                for (int cc = 0; cc < c; cc++) {
+                    for (int rr = 0; rr < r; rr++) {
+                        Rect rect = new Rect(offsetx + cc * width, offsety + rr * height, offsetx + (cc + 1) * width - 1, offsety + (rr + 1) * height - 1);
+                        //Toast.makeText(this, "" + rect, Toast.LENGTH_SHORT).show();
+                        canvas.drawRect(rect, paint);
+                    }
+                }
+
+
+                if (paint.getTextSize() != 20.f) paint.setTextSize(20.0f);
+                canvas.drawText("" + mScaleFactor + " " + mRotate + " " + mPosX + "," + mPosY, 0, getHeight() - 20, paint);
+
+            }
+
+        } catch (Exception ex) {
+
+            Toast.makeText(this.getContext(), ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+
+        if (stateLocked) return true;
+
+        // Let the ScaleGestureDetector inspect all events.
+
+
+        mScaleDetector.onTouchEvent(ev);
+
+
+        final int action = ev.getAction();
+        switch (action & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN: {
+                final float x = ev.getX();
+                final float y = ev.getY();
+                mLastTouchX = x;
+                mLastTouchY = y;
+                mActivePointerId = ev.getPointerId(0);
+                break;
+            }
+
+            case MotionEvent.ACTION_MOVE: {
+                final int pointerIndex = ev.findPointerIndex(mActivePointerId);
+                final float x = ev.getX(pointerIndex);
+                final float y = ev.getY(pointerIndex);
+
+                // Only move if the ScaleGestureDetector isn't processing a gesture.
+                // if (!mScaleDetector.isInProgress()) {
+                if (true) {
+                    final float dx = x - mLastTouchX;
+                    final float dy = y - mLastTouchY;
+
+                    mPosX += dx;
+                    mPosY += dy;
+
+                    invalidate();
+                }
+
+                mLastTouchX = x;
+                mLastTouchY = y;
+
+                break;
+            }
+
+            case MotionEvent.ACTION_UP: {
+                mActivePointerId = INVALID_POINTER_ID;
+                break;
+            }
+
+            case MotionEvent.ACTION_CANCEL: {
+                mActivePointerId = INVALID_POINTER_ID;
+                break;
+            }
+
+            case MotionEvent.ACTION_POINTER_UP: {
+                final int pointerIndex = (ev.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK)
+                        >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+                final int pointerId = ev.getPointerId(pointerIndex);
+                if (pointerId == mActivePointerId) {
+                    // This was our active pointer going up. Choose a new
+                    // active pointer and adjust accordingly.
+                    final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+                    mLastTouchX = ev.getX(newPointerIndex);
+                    mLastTouchY = ev.getY(newPointerIndex);
+                    mActivePointerId = ev.getPointerId(newPointerIndex);
+                }
+                break;
+            }
+        }
+
+
+        return true;
+    }
+
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            if (!rotating) {
+                mScaleFactor *= detector.getScaleFactor();
+                // Don't let the object get too small or too large.
+                mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 10.0f));
+                //   Toast.makeText(PictureView.this.getContext(), "" + detector.getScaleFactor(), Toast.LENGTH_SHORT).show();
+
+            } else {
+
+                mRotate += ROTATION_MULTIPLIER * (detector.getScaleFactor() - 1);
+
+            }
+            invalidate();
+            return true;
+        }
+    }
+}
